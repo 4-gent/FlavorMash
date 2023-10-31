165607ce06f6dde20e06b589c57ed4254dc92477
Ted4523 <114703222+Ted4523@users.noreply.github.com>
2023-10-31 11:43:12 -0700

    updated dish class
    added calculate() method

--- components/Dish.java
+++ components/Dish.java
@@ -1,8 +1,9 @@
 package components;
-import java.util.ArrayList;
+import java.util.*;
 public class Dish {
     private ArrayList<ArrayList<Attribute>> attributes;
     private String address;
+    private double score;
     /**
      * Set attributes to static size of 5 for each of the attributes (Distance, Flavor, FoodGenre, PortionSize, Price)
      * Each will have an arrayList to account for multiple flavors etc.
@@ -10,6 +11,24 @@ public class Dish {
      */
     
     Dish(){
-        attributes = new ArrayList<ArrayList<Attribute>>(5);
+        attributes = new ArrayList<>(5);
+
     }
+
+
+    public void addAttribute(ArrayList<Attribute> attr){
+        attributes.add(attr);
+    }
+
+    public double calculate(){
+        double score = 0.0;
+        for(ArrayList<Attribute> a: attributes){
+            for(Attribute x:a){
+                score+=x.getWeight();
+            }
+        }
+        return score;
+    }
+
+
 }
