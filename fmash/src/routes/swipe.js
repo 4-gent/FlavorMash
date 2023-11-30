63c3cb33ee140228835d181cb2ae14ea5f1fedb1
Marlon (Mj) Burog <spicyrice09@gmail.com>
2023-11-30 06:53:46 -0800

    Add files via upload

--- fmash/src/routes/swipe.js
+++ fmash/src/routes/swipe.js
@@ -1,6 +1,12 @@
 import React from "react";
 import SwipeCarousel from '../components/carousel';
 import Navigation from '../components/Navigation';
+import ham from '../public/images/ham.jpg';
+import marg from '../public/images/marg.jpg';
+import chick from '../public/images/chick.jpg';
+import pork from '../public/images/pork.jpg';
+import fett from '../public/images/fett.jpg';
+
 
 function changeBackground(color) {
     document.body.style.background = color;
@@ -15,8 +21,19 @@ function Swipe(){
             <nav>
                 <Navigation />
             </nav>
-            <SwipeCarousel />
-            
+            <SwipeCarousel 
+                imgOne={ham} 
+                imgTwo={marg} 
+                imgThree={chick} 
+                imgFour={pork} 
+                imgFive={fett} 
+
+                nameOne={'Ham and Cheese Croquette'}
+                nameTwo={'Margherita Pizza'}
+                nameThree={'Chicken Kalguksu'}
+                nameFour={'Pork Chop'}
+                nameFive={'Fettucine'}
+            />
         </div>
     )
 }
